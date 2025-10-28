d947a6fd5a44252ea9eec2d016a98b83f944dced
Muhammad Sax <mohammedsadawi10@gmail.com>
2025-10-28 19:47:48 +0200

    files desgined as MVC

---
+++ src/model/Task.java
@@ -0,0 +1,11 @@
+package src.model;
+
+public class Task {
+    private String id;
+    private String title;
+    private String description;
+    private Date dueDate;
+    private TaskPriority priority;
+    private boolean completed;
+    // + getters/setters
+}
